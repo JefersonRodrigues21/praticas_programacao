0400f8d8ab7632eb363ec9fd10be52048949972c
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-02-12 19:13:01 -0300

    exerciciio soma js

---
+++ aula.js
@@ -0,0 +1,28 @@
+// string
+// const nome =  prompt('Por favor, digite o nome')
+const n1 = 10;
+const n2 = 7;
+const aprovado = true;
+const minimo = 9;
+
+// operadores
+console.log('Operadores aritméticos');
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(n1 % n2);
+
+console.log('Operadores Relacionais');
+console.log(n1 == n2);
+console.log(n1 != n2);
+console.log(n1 < n2);
+console.log(n1 > n2);
+console.log(n1 <= n2);
+console.log(n1 >= n2);
+
+console.log('Operadores Lógicos');
+console.log(aprovado);
+console.log(!aprovado);
+console.log(n2 >= 7 && n2 >= minimo); // e
+console.log(n2 >= 7 || n2 >= minimo); // ou
